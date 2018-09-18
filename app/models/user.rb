b9804d56bb8e565a198a1c8d01e28551bf19e62d
Aliaksei Shvants <ashvants91@gmail.com>
2018-09-19 02:14:37 +0300

    add validate rules for name and email

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /[\w+\-.]+@[a-z\d\-.]+\.[a-z]+/i
+  before_save { self.email = email.downcase }
+
+  validates :first_name, presence: true, length: { maximum: 20 }
+  validates :last_name,  presence: true, length: { maximum: 20 }
+  validates :email,      presence: true, length: { maximum: 255 },
+                         format: { with: VALID_EMAIL_REGEX },
+                         uniqueness: { case_sensitive: false }
+end
