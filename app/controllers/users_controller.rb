ea0e59d1d0ca32e6ec961859c72f65e9e081667a
Aliaksei Shvants <ashvants91@gmail.com>
2018-09-22 11:43:35 +0300

    show users tasks in user 'my task' link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,12 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: [:destroy]
 
   def index
-    @users = User.paginate(:page => params[:page])
+    @users = User.paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
-    @title = "My tasks"
+    @tasks = @user.tasks.paginate(page: params[:page])
   end
 
   def new
